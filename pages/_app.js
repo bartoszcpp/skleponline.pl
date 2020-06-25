9d06ee1215963b339b3227dd26a2108a31df2061
Your Name <you@example.com>
2020-06-25 16:47:51 +0200

    25-06-01

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ import ApolloClient from "apollo-boost";
 import { AppProvider } from "../components/contex/AppContex";
 
 const client = new ApolloClient({
-  uri: "http://www.bartcpp.ssd-linuxpl.com/graphql",
+  uri: "https://www.bartcpp.ssd-linuxpl.com/graphql",
 });
 
 // This default export is required in a new `pages/_app.js` file.
